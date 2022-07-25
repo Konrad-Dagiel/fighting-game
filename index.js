7f0b8956759f89a7d70c4c3daef2a25606fe882c
Konrad-Dagiel <kdawgy9@gmail.com>
2022-07-25 15:25:11 +0100

    added game over and timer

--- index.js
+++ index.js
@@ -152,6 +152,11 @@ function animate(){
         player.health -= 20;
         document.querySelector('#playerHealth').style.width = player.health + '%';
         enemy.isAttacking=false;
+        
+    }
+    //end game based on hp
+    if (enemy.health <=0 || player.health <=0){
+        determineWinner({player, enemy, timerId})
     }
 }
 
@@ -161,6 +166,38 @@ function rectangularCollision({rectangle1, rectangle2}){
         && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
         && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
 }
+
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    if (player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML='Tie';
+        
+    }
+    else if (player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML='Player 1 Wins!';
+
+    }
+    else if (player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML='Player 2 Wins!';
+
+    }
+    document.querySelector('#displayText').style.display='flex';
+
+}
+
+let timer = 60;
+let timerId;
+function decreaseTimer(){
+    timerId = setTimeout(decreaseTimer, 1000)
+    if (timer >0){
+    timer-=1;
+    document.querySelector('#timer').innerHTML = timer;
+    }
+    if (timer === 0){
+        determineWinner({player, enemy, timerId})
+    }
+}
+decreaseTimer()
 
 animate();
 //event listeners
